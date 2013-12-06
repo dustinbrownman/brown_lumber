40caee1ac9b27753a33a568bd01699790e5fa8da
Dustin Brown <dbrown9@gmail.com>
2013-12-06 15:43:09 -0800

    make main categories editable

--- app/controllers/main_categories_controller.rb
+++ app/controllers/main_categories_controller.rb
@@ -1,4 +1,6 @@
 class MainCategoriesController < ApplicationController
+	before_filter :authenticate_admin!, only: [:edit, :update]
+
 	add_breadcrumb "Categories", :main_categories_path
 
 	def index
@@ -9,5 +11,25 @@ class MainCategoriesController < ApplicationController
 		@main_category = MainCategory.find(params[:id])
 		@main_categories = MainCategory.all
 		add_breadcrumb @main_category.name, @main_category
+	end
+
+	def edit
+		@main_category = MainCategory.find(params[:id])
+	end
+
+	def update
+		@main_category = MainCategory.find(params[:id])
+
+		if @main_category.update(main_category_params)
+			redirect_to @main_category, notice: 'Category successfully updated!'
+		else
+			render :edit
+		end
+	end
+
+private
+
+	def main_category_params
+		params.require(:main_category).permit(:name, :description)
 	end
 end
--- spec/features/category_pages_spec.rb
+++ spec/features/category_pages_spec.rb
@@ -52,4 +52,65 @@ describe 'Main Category Pages' do
 		# May need to add this back in if we decide we want products listed on this page
 		# it { should have_content @product.description }
 	end
+
+	describe 'edit page' do
+		let(:admin) { FactoryGirl.create(:admin) }
+
+		before do
+			@product = FactoryGirl.create(:product)
+			@sub_category = @product.sub_category
+			@main_category = @sub_category.main_category
+		end
+
+		describe 'edit link' do
+			it "should be accessbile to signed in admin" do
+				sign_in(admin)
+				visit main_category_path(@main_category)
+				page.should have_link 'edit'
+			end
+
+			it "should not be accessbile others" do
+				visit main_category_path(@main_category)
+				page.should_not have_content 'edit'
+			end
+		end
+
+		describe "visiting edit path" do
+			before { visit edit_main_category_path(@main_category) }
+
+			context "as admin" do
+
+			end
+
+			context "as visitor" do
+				it { should have_content 'You need to sign in' }
+			end
+		end
+
+		describe 'updating category' do
+			before do
+				sign_in(admin)
+				visit edit_main_category_path(@main_category)
+			end
+
+			context 'with valid information' do
+				before do
+					fill_in 'Name', with: 'New name'
+					fill_in 'Description', with: 'New description'
+					click_on 'Update'
+				end
+
+				it { should have_content 'successfully' }
+			end
+
+			context 'wtih invalid information' do
+				before do
+					fill_in 'Name', with: ''
+					click_on 'Update'
+				end
+
+				it { should have_content 'blank' }
+			end
+		end
+	end
 end
