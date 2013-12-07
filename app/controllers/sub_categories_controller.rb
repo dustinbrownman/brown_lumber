75526d1d11bdf75225724979aa0afd57365689c5
Dustin Brown <dbrown9@gmail.com>
2013-12-06 16:04:43 -0800

    make subcategories editable

--- app/controllers/sub_categories_controller.rb
+++ app/controllers/sub_categories_controller.rb
@@ -1,4 +1,5 @@
 class SubCategoriesController < ApplicationController
+	before_filter :authenticate_admin!, only: [:edit, :update]
 
 	def show
 		@main_categories = MainCategory.all
@@ -6,5 +7,25 @@ class SubCategoriesController < ApplicationController
 		add_breadcrumb "Categories", main_categories_path
 		add_breadcrumb @sub_category.main_category.name, @sub_category.main_category
 		add_breadcrumb @sub_category.name, @sub_category
+	end
+
+	def edit
+		@sub_category = SubCategory.find(params[:id])
+	end
+
+	def update
+		@sub_category = SubCategory.find(params[:id])
+
+		if @sub_category.update(main_category_params)
+			redirect_to @sub_category, notice: 'Category successfully updated!'
+		else
+			render :edit
+		end
+	end
+
+private
+
+	def main_category_params
+		params.require(:sub_category).permit(:name, :description)
 	end
 end
--- spec/features/sub_categories_pages_spec.rb
+++ spec/features/sub_categories_pages_spec.rb
@@ -14,4 +14,64 @@ describe "SubCategory pages" do
 		it { should have_content @sub_category.description }
 		it { should have_content @product.description }
 	end
+
+	describe 'edit page' do
+		let(:admin) { FactoryGirl.create(:admin) }
+
+		before do
+			@product = FactoryGirl.create(:product)
+			@sub_category = @product.sub_category
+		end
+
+		describe 'edit link' do
+			it "should be accessbile to signed in admin" do
+				sign_in(admin)
+				visit sub_category_path(@sub_category)
+				page.should have_link 'edit'
+			end
+
+			it "should not be accessbile others" do
+				visit sub_category_path(@sub_category)
+				page.should_not have_content 'edit'
+			end
+		end
+
+		describe "visiting edit path" do
+			before { visit edit_sub_category_path(@sub_category) }
+
+			context "as admin" do
+
+			end
+
+			context "as visitor" do
+				it { should have_content 'You need to sign in' }
+			end
+		end
+
+		describe 'updating category' do
+			before do
+				sign_in(admin)
+				visit edit_sub_category_path(@sub_category)
+			end
+
+			context 'with valid information' do
+				before do
+					fill_in 'Name', with: 'New name'
+					fill_in 'Description', with: 'New description'
+					click_on 'Update'
+				end
+
+				it { should have_content 'successfully' }
+			end
+
+			context 'wtih invalid information' do
+				before do
+					fill_in 'Name', with: ''
+					click_on 'Update'
+				end
+
+				it { should have_content 'blank' }
+			end
+		end
+	end
 end
