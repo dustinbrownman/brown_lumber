e0b9789e26904b7045fc1e28823a84d1165951ba
Dustin Brown <dbrown9@gmail.com>
2013-12-06 16:52:16 -0800

    make products editable (lacking tests)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+	before_filter :authenticate_admin!, only: [:edit, :update]
+
 	def show
 		@main_categories = MainCategory.all
 		@product = Product.find(params[:id])
@@ -6,5 +8,26 @@ class ProductsController < ApplicationController
 		add_breadcrumb @product.sub_category.main_category.name, @product.sub_category.main_category
 		add_breadcrumb @product.sub_category.name, @product.sub_category
 		add_breadcrumb @product.description, @product
+	end
+
+	def edit
+		@product = Product.find(params[:id])
+	end
+
+	def update
+		@product = Product.find(params[:id])
+
+		if @product.update(product_params)
+			redirect_to @product.sub_category, notice: 'Product successfully updated!'
+		else
+			render :edit
+		end
+	end
+
+private
+
+	def product_params
+		params.require(:product).permit(:description, :part_number, :barcode_number,
+																		:units, :weight, :length)
 	end
 end
